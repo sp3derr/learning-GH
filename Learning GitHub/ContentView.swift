6f4fc499681bdc694baacc5c6c1cd05dbd14bb30
Jacob Boyd <jacoboyd10@gmail.com>
2021-11-29 08:44:30 -0600

    Initial Commit

---
+++ Learning GitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning GitHub
+//
+//  Created by AM AWK - Jesse Renicker on 11/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning GitHub
+//
+//  Created by AM AWK - Jesse Renicker on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
